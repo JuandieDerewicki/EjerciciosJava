48e8dd5844c971b7a800e4505a8cc18bfd8a9c9e
Juan Diego Derewicki <88844221+JuandieDerewicki@users.noreply.github.com>
2021-09-03 12:17:17 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Desafio 06 / Ejercicio combinado
+package desafios;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double ejercicioCombinado = (((6.0 / 5.0) - (2.0 / 3.0) * (7.0 / 2.0) + (2.0 / 30.0)) / (1.0 / 3.0) / 5.0);
+        System.out.println("El resultado de la operacion combinada es " + ejercicioCombinado );
+    }
+}
